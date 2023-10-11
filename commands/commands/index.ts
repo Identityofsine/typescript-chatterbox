c10e191eec395a594cd801f7524f3e97a32c9e26
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-10-11 02:07:07 -0400

    queue?

--- commands/commands/index.ts
+++ commands/commands/index.ts
@@ -2,6 +2,7 @@ import { join } from "./join";
 import { leave } from "./leave";
 import { ping } from "./ping";
 import { play } from "./play";
+import { skip } from "./skip";
 import { stop } from "./stop";
 
-export const commands = [ping, join, leave, play, stop];
+export const commands = [ping, join, leave, play, stop, skip];
